cd0ff900f2789e0e5149544274d649b290ad9d9b
rahul07sharmars <rahul07sharmars@gmail.com>
2024-10-02 00:28:50 +0530

    init

--- src/main/java/com/example/shareexpenese/ShareExpeneseApplication.java
+++ src/main/java/com/example/shareexpenese/ShareExpeneseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShareExpeneseApplication {
 
     public static void main(String[] args) {
+        System.out.println("test");
         SpringApplication.run(ShareExpeneseApplication.class, args);
     }
 
