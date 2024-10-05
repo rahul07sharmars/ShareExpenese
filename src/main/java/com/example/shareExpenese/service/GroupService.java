22dce6bc6e64d22d610c83f81020185fddfa16dc
rahul07sharmars <rahul07sharmars@gmail.com>
2024-10-05 10:41:07 +0530

    group upsert api

--- src/main/java/com/example/shareExpenese/service/GroupService.java
+++ src/main/java/com/example/shareExpenese/service/GroupService.java
@@ -1,2 +1,10 @@
-package com.example.shareExpenese.service;public class GroupService {
+package com.example.shareExpenese.service;
+
+import com.example.shareExpenese.common.ApiResponse;
+import com.example.shareExpenese.entity.Group;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+public interface GroupService {
+
+    ApiResponse<Group> upsertGroup(Group group) throws JsonProcessingException;
 }
--- src/main/java/com/example/shareExpenese/service/GroupServiceImpl.java
+++ src/main/java/com/example/shareExpenese/service/GroupServiceImpl.java
@@ -1,2 +1,36 @@
-package com.example.shareExpenese.service;public class GroupServiceImpl {
+package com.example.shareExpenese.service;
+
+import com.example.shareExpenese.common.ApiResponse;
+import com.example.shareExpenese.entity.Group;
+import com.example.shareExpenese.entity.User;
+import com.example.shareExpenese.repository.GroupRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GroupServiceImpl implements GroupService {
+
+    @Autowired
+    private GroupRepository groupRepository;
+    private static final Logger LOG = LoggerFactory.getLogger(GroupServiceImpl.class);
+    public ApiResponse<Group> upsertGroup(Group group) {
+        try {
+            Group updateGroup= groupRepository.save(group);
+            if( group.getId()!=null){
+                LOG.info("Group Created");
+                return new ApiResponse<>(200, "Group Created", updateGroup);
+            }
+            else {
+                LOG.info("Group Updated");
+                return new ApiResponse<>(200, "Group Updated", updateGroup);
+            }
+
+        } catch (Exception e) {
+            return new ApiResponse<>(400, e.getMessage());
+        }
+    }
+    
+
 }
